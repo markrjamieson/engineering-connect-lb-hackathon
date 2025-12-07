3dff64c0c4deb4410a8cbc057ee6a172f6ea38af
markrjamieson <jamieson.m@protonmail.com>
2025-12-07 06:09:46 +0000

    feat: initial commit - change name of app in main.py and enter team name for submission folder

--- submissions/504/main.py
+++ submissions/504/main.py
@@ -2,13 +2,13 @@ import os
 
 from flask import Flask, jsonify
 
-app = Flask(__name__)
+lb = Flask(__name__)
 
 
-@app.route("/", methods=["GET"])
+@lb.route("/", methods=["GET"])
 def get_tasks():
     return jsonify({"message": "Hello World"})
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=os.environ.get("PORT", 5000))
+    lb.run(debug=True, host="0.0.0.0", port=os.environ.get("PORT", 5000))
